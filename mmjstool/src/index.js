f608bedd2afaedff168df64bb78d61db9635628d
DNT <ducconit@gmail.com>
2022-10-03 09:29:04 +0700

    refactor: remove license mattermost

--- mmjstool/src/index.js
+++ mmjstool/src/index.js
@@ -1,5 +1,3 @@
-// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
-// See LICENSE.txt for license information.
 const yargs = require('yargs');
 
 const i18nHandlers = require('./i18n_handlers');
