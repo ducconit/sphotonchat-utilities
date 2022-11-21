dc439ba4e582b39833313bfd980c66fd777c65b4
DNT <ducconit@gmail.com>
2022-11-21 19:03:54 +0700

    feat: add progess bar to console

--- mmgotool/commands/i18n.go
+++ mmgotool/commands/i18n.go
@@ -15,6 +15,8 @@ import (
 	"sort"
 	"strings"
 
+	translate "github.com/ducconit/go-translate"
+	progressbar "github.com/schollz/progressbar/v3"
 	"github.com/spf13/cobra"
 )
 
@@ -28,6 +30,15 @@ type Translation struct {
 type Item struct {
 	ID          string          `json:"id"`
 	Translation json.RawMessage `json:"translation"`
+}
+
+type convertToViFromEn struct {
+	replace map[string]string
+	ids     []string
+	srcEn   map[string]interface{}
+	srcVi   map[string]interface{}
+	bar     *progressbar.ProgressBar
+	t       *translate.GoogleTranslate
 }
 
 var I18nCmd = &cobra.Command{
@@ -67,6 +78,12 @@ var CleanEmptyCmd = &cobra.Command{
 	RunE:    cleanEmptyCmdF,
 }
 
+var ToVi = &cobra.Command{
+	Use:   "tovi",
+	Short: "convert en to vi",
+	RunE:  toViF,
+}
+
 func init() {
 	ExtractCmd.Flags().Bool("skip-dynamic", false, "Whether to skip dynamically added translations")
 	ExtractCmd.Flags().String("portal-dir", "../customer-web-server", "Path to folder with the sPhoton Chat Customer Portal source code")
@@ -89,11 +106,15 @@ func init() {
 	CleanEmptyCmd.Flags().String("enterprise-dir", "../enterprise", "Path to folder with the sPhoton Chat enterprise source code")
 	CleanEmptyCmd.Flags().String("mattermost-dir", "./", "Path to folder with the sPhoton Chat source code")
 
+	ToVi.Flags().String("translation-dir", "../server/i18n", "Path to folder with the sPhoton Chat server translate")
+	ToVi.Flags().String("replace", "", "Path to file of list string replace")
+
 	I18nCmd.AddCommand(
 		ExtractCmd,
 		CheckCmd,
 		CheckEmptySrcCmd,
 		CleanEmptyCmd,
+		ToVi,
 	)
 	RootCmd.AddCommand(I18nCmd)
 }
@@ -686,4 +707,140 @@ func JSONMarshal(t interface{}) ([]byte, error) {
 	encoder.SetIndent("", "    ")
 	err := encoder.Encode(t)
 	return buffer.Bytes(), err
+}
+
+func toViF(command *cobra.Command, args []string) error {
+	translationDir, err := command.Flags().GetString("translation-dir")
+	if err != nil {
+		return errors.New("invalid translation-dir parameter")
+	}
+	replaceFile, err := command.Flags().GetString("replace")
+	if err != nil {
+		return errors.New("invalid replace parameter")
+	}
+
+	converter := &convertToViFromEn{}
+	if replaceFile != "" {
+		replaceF, err := os.ReadFile(replaceFile)
+		if err != nil {
+			return err
+		}
+		err = json.Unmarshal(replaceF, &converter.replace)
+		if err != nil {
+			return err
+		}
+	}
+
+	enFile, err := os.ReadFile(path.Join(translationDir, "en.json"))
+	if err != nil {
+		return err
+	}
+
+	en := []Translation{}
+	err = json.Unmarshal(enFile, &en)
+	if err != nil {
+		return err
+	}
+
+	src := make(map[string]interface{})
+	ids := []string{}
+
+	for _, v := range en {
+		src[v.Id] = v.Translation
+		ids = append(ids, v.Id)
+	}
+
+	converter.ids = ids
+	converter.srcEn = src
+	converter.bar = progressbar.Default(int64(len(ids)))
+	converter.t = translate.NewGoogleTranslate()
+
+	viFile, err := os.ReadFile(path.Join(translationDir, "vi.json"))
+	if err != nil {
+		return err
+	}
+
+	vi := []Translation{}
+	err = json.Unmarshal(viFile, &vi)
+	if err != nil {
+		return err
+	}
+
+	srcVi := make(map[string]interface{})
+	for _, v := range vi {
+		srcVi[v.Id] = v.Translation
+	}
+
+	converter.srcVi = srcVi
+	return converter.Write(path.Join(translationDir, "vi.json"))
+}
+
+func (c *convertToViFromEn) Write(file string) error {
+	result := make([]Translation, 0)
+	for _, id := range c.ids {
+		c.bar.Add(1)
+		value, ok := c.srcVi[id]
+		if !ok {
+			v, ok := c.srcEn[id]
+			if !ok {
+				panic("missing key: " + id)
+			}
+			v, err := c.replaceString(v)
+			if err != nil {
+				return err
+			}
+			v, err = c.replaceMap(v)
+			if err != nil {
+				return err
+			}
+			value = v
+		}
+		result = append(result, Translation{
+			Id:          id,
+			Translation: value,
+		})
+	}
+	content, err := json.MarshalIndent(result, "", "  ")
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(file, content, 0666)
+}
+
+func (c *convertToViFromEn) replaceString(value interface{}) (interface{}, error) {
+	if v, ok := value.(string); ok {
+		if len(strings.TrimSpace(v)) == 0 {
+			return v, nil
+		}
+		text, err := c.t.Translate(v)
+		if err != nil {
+			return nil, err
+		}
+		for search, replace := range c.replace {
+			text = strings.ReplaceAll(text, search, replace)
+		}
+		return text, nil
+	}
+	return value, nil
+}
+
+func (c *convertToViFromEn) replaceMap(input interface{}) (interface{}, error) {
+	if rows, ok := input.(map[string]interface{}); ok {
+		result := make(map[string]interface{})
+		for id, value := range rows {
+			if valueStr, ok := value.(string); ok {
+				text, err := c.t.Translate(valueStr)
+				if err != nil {
+					return nil, err
+				}
+				for search, replace := range c.replace {
+					text = strings.ReplaceAll(text, search, replace)
+				}
+				result[id] = text
+				continue
+			}
+		}
+		return result, nil
+	}
+	return input, nil
 }
--- mmgotool/go.mod
+++ mmgotool/go.mod
@@ -3,15 +3,18 @@ module code.sphoton.com/sphotonchat/utilities/mmgotool
 go 1.18
 
 require (
-	github.com/bregydoc/gtranslate v0.0.0-20200913051839-1bd07f6c1fc5
+	github.com/ducconit/go-translate v0.0.0-20221121162855-8e4a8396ccc9
+	github.com/schollz/progressbar/v3 v3.12.1
 	github.com/spf13/cobra v1.5.0
-	golang.org/x/text v0.4.0
 )
 
 require (
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
-	github.com/robertkrimen/otto v0.0.0-20221025135307-511d75fba9f8 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
+	github.com/rivo/uniseg v0.4.2 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/stretchr/testify v1.8.1 // indirect
-	gopkg.in/sourcemap.v1 v1.0.5 // indirect
+	golang.org/x/sys v0.1.0 // indirect
+	golang.org/x/term v0.1.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
 )
